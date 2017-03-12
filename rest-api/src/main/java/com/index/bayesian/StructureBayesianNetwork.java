5fd3f37532bc00e28acb4717a969be85776c7dd5
jacr <jacr@st-andrews.ac.uk>
2017-03-12 12:57:47 +0000

    intial changes across from the other branch, to set up and allow for baysien network.

--- rest-api/src/main/java/com/index/bayesian/StructureBayesianNetwork.java
+++ rest-api/src/main/java/com/index/bayesian/StructureBayesianNetwork.java
@@ -12,6 +12,8 @@ public class StructureBayesianNetwork {
 
     private static final String userNodeName = "user";
 
+    private static final String groupNodeName = "group";
+
     private static final String structureNodeName = "structureChoice";
 
     private static final String userNodeStart = "user_";
@@ -38,8 +40,9 @@ public class StructureBayesianNetwork {
         this.network = new Network();
         network.addNode(Network.NodeType.Cpt, structureNodeName);
         network.addNode(Network.NodeType.Cpt, userNodeName);
-
+        network.addNode(Network.NodeType.Cpt, groupNodeName);
         addOutcomesToNetwork();
+
         // || data.getUserIdTotalDecisions().get(data.getUserId()) == null
         if(data.getUserIdTotalDecisions().size() == 1){
             // No need for user node as no choice to be made!
@@ -48,9 +51,21 @@ public class StructureBayesianNetwork {
             // By default the node must have two outcomes. Remove the default outcomes.
             network.deleteOutcome(userNodeName, 0);
             network.deleteOutcome(userNodeName, 0);
-            setUserNodeDef();
+            simpleSetNodeDef(data.getUserIdTotalDecisions(), userNodeName);
             network.addArc(userNodeName, structureNodeName);
         }
+
+        if(data.getGroupIdTotalDecisions().size() == 1){
+            // No need for user node as no choice to be made!
+            network.deleteNode(groupNodeName);
+        }else{
+            // By default the node must have two outcomes. Remove the default outcomes.
+            network.deleteOutcome(groupNodeName, 0);
+            network.deleteOutcome(groupNodeName, 0);
+            simpleSetNodeDef(data.getGroupIdTotalDecisions(), groupNodeName);
+            network.addArc(groupNodeName, structureNodeName);
+        }
+
         // remove the default structures.
         network.deleteOutcome(structureNodeName, 0);
         network.deleteOutcome(structureNodeName, 0);
@@ -60,52 +75,57 @@ public class StructureBayesianNetwork {
     }
 
     private void addOutcomesToNetwork(){
-
         for ( String key : data.getStructNodeNameToSmiles().keySet()) {
             network.addOutcome(structureNodeName, key);
         }
-
         for ( Integer key : data.getUserIdTotalDecisions().keySet()) {
             network.addOutcome(userNodeName, userNodeStart + key);
         }
+        for ( Integer key : data.getGroupIdTotalDecisions().keySet()) {
+            network.addOutcome(groupNodeName, groupNodeStart + key);
+        }
     }
 
-    private void setUserNodeDef(){
-        double[] userDefinition = new double[data.getUserIdTotalDecisions().size()];
+    private void simpleSetNodeDef(Map<Integer, Long> map, String nodeName){
+        double[] definitions = new double[map.size()];
         int i = 0;
-        for(Map.Entry<Integer, Long> userEntry : data.getUserIdTotalDecisions().entrySet()){
+        for(Map.Entry<Integer, Long> entry : map.entrySet()){
             // The number of times a user has made a choice / total times a choice has been made.
-            userDefinition[i++] = (double) userEntry.getValue() / (double) data.getTotalDecisions();
+            definitions[i++] = (double) entry.getValue() / (double) data.getTotalDecisions();
         }
-        network.setNodeDefinition(userNodeName, userDefinition);
+        network.setNodeDefinition(nodeName, definitions);
     }
 
     private void setStructureChoiceDef(){
 
-        List<EdgeMetadata> edges = data.getEdges();
         Map<String, Long> smilesTos = data.getSmilesToTotalPicks();
         Map<Integer, Long> userIds = data.getUserIdTotalDecisions();
-
+        Map<Integer, Long> groupIds = data.getGroupIdTotalDecisions();
+        Map<String, Long> userIdsSmilesTo = data.getUserIdSmilesToPicks();
         double totalDecisions = data.getTotalDecisions();
 
-        double[] structureDefinition = new double[edges.size()];
+        double[] structureDefinition = new double[smilesTos.size() * userIds.size() * groupIds.size()];
         int defIndex = 0;
-        for (EdgeMetadata edge : edges) {
+        for (Map.Entry<Integer, Long> userId : userIds.entrySet()) {
+            for (Map.Entry<Integer, Long> groupId : groupIds.entrySet()) {
+                for (Map.Entry<String, Long> smilesTo : smilesTos.entrySet()) {
 
-            double smilesToTotalPicks = smilesTos.get(edge.getSmilesTo());
-            double userTotalPicks = userIds.get(edge.getUserId());
+                    // P(A | B) = ( P(B | A) * P(A) ) / P(B)
+                    double userIdSmiles = userIdsSmilesTo.get(userId.getKey().toString()+smilesTo.getKey());
 
-            // P(A | B) = ( P(B | A) * P(A) ) / P(B)
-            double probOfThisUserGivenThisSmileTo = (double) edge.getTimes() / smilesToTotalPicks;
-            double probOfThisSmileTo = smilesToTotalPicks / totalDecisions;
-            double probOfThisUser = userTotalPicks / totalDecisions;
+                    double P_BGivenA =  userIdSmiles / smilesTo.getValue();
+                    System.out.println(P_BGivenA);
+                    double P_A = smilesTo.getValue() / totalDecisions;
+                    double P_B = userId.getValue() / totalDecisions;
 
-            if (probOfThisUser != 1) {
-                structureDefinition[defIndex] = (probOfThisUserGivenThisSmileTo * probOfThisSmileTo) / probOfThisUser;
-            }else{
-                structureDefinition[defIndex] = probOfThisSmileTo;
+                    if (P_B != 1) {
+                        structureDefinition[defIndex] = (P_BGivenA * P_A) / P_B;
+                    }else{
+                        structureDefinition[defIndex] = P_A;
+                    }
+                    defIndex++;
+                }
             }
-            defIndex++;
         }
         network.setNodeDefinition(structureNodeName, structureDefinition);
     }
@@ -124,7 +144,15 @@ public class StructureBayesianNetwork {
             result.add(new SmilesToProb(list.get(0), 1));
             return result;
         } else if (network.getNodeCount() > 1) {
-            network.setEvidence(userNodeName, userNodeStart + data.getUserId());
+            String nodes[] = network.getParentIds(structureNodeName);
+            for (String node: nodes) {
+                if (node.equals(userNodeName)){
+                    network.setEvidence(userNodeName, userNodeStart + data.getUserId());
+                }
+                if (node.equals(groupNodeName)){
+                    network.setEvidence(groupNodeName, groupNodeStart + data.getGroupId());
+                }
+            }
             network.updateBeliefs();
             probs = network.getNodeValue(structureNodeName);
         }else{
@@ -143,3 +171,4 @@ public class StructureBayesianNetwork {
         return result;
     }
 }
+    
