7d0c97d462e8a4266b28e7778f438327512afa1f
sic2 <sic2@st-andrews.ac.uk>
2017-09-29 11:44:29 +0100

    delete data

--- study-data/getDump.sh
+++
@@ -1,2 +0,0 @@
-mysqldump --opt -u jacr -h jacr.host.cs.st-andrews.ac.uk -pqU2ZcSJh8b!e1W jacr_structure_prediction > new_data.sql
-
--- study-data/getTable.sh
+++
@@ -1,2 +0,0 @@
- mysql -u jacr -h jacr.host.cs.st-andrews.ac.uk -pqU2ZcSJh8b!e1W jacr_structure_prediction -e "select *, TIMEDIFF(end_time, start_time) as time_taken from study_data order by user_id, smiles;" -B > study_data_table.tsv
-
